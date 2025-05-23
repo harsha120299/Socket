33450053dea5367878059dd80a2d1163e8500a13
harsha120299 <harshagallage1202@gmail.com>
2025-05-23 11:17:08 +0530

    first commit

--- ClientInitializer2.java
+++ ClientInitializer2.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientInitializer2 {
+    public static void main(String[] args) throws IOException {
+        try{
+            Socket socket = new Socket("localhost", 3000);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+
+            String massage="";
+            while (true){
+                System.out.print("Enter your massage: ");
+                massage = scanner.nextLine();
+                out.writeUTF(massage);
+                out.flush();
+
+                String response = in.readUTF();
+
+                if(response.equals("exit")){
+                    System.out.println("You have been exited");
+                    break;
+                }else {
+                    System.out.println("Server response : "+response);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
 }
--- ServerInitializer.java
+++ ServerInitializer.java
@@ -1,4 +1,81 @@
-package PACKAGE_NAME;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public class ServerInizializer {
+
+public class ServerInitializer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+    private boolean serverRunning = true;
+    private static String upTime;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try{
+                ServerSocket serverSocket = new ServerSocket(3000);
+                upTime = LocalTime.now().toString();
+                boolean serverRunning = true;
+                while (serverRunning) {
+                    Socket clientSocket = serverSocket.accept();
+                    new ClientHandler(clientSocket).start();
+                }
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static class ClientHandler extends Thread {
+
+        private Socket clientSocket;
+        private DataInputStream in;
+        private DataOutputStream out;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try{
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            String message = in.readUTF();
+
+            switch (message) {
+                case "TIME":
+                    out.writeUTF(LocalTime.now().toString());
+                    out.flush();
+                    break;
+                case "DATE":
+                    out.writeUTF(LocalDate.now().toString());
+                    out.flush();
+                    break;
+                case "UPTIME":
+                    out.writeUTF(upTime);
+                    out.flush();
+                    break;
+                case "BYE":
+                    clientSocket.close();
+                    break;
+                case "HELP":
+                    out.writeUTF("These are available Commands TIME/DATE/UPTIME/BYE");
+                    out.flush();
+                default:
+                    out.writeUTF(message);
+                    out.flush();
+
+            }
+        }catch(IOException e){
+            e.printStackTrace();}
+        }
+    }
 }
